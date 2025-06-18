937065526c58d8ff44fc376cef96c4bc3d7ee8a0
Prakhar <mishraprakhar150@gmail.com>
2025-06-18 19:03:31 +0530

    merge resolved

--- api/index.js
+++ api/index.js
@@ -5,13 +5,6 @@ require('dotenv').config();
 const authRoutes = require('./routes/auth')
 const teamRoutes = require('./routes/team')
 const githubRoutes = require('./routes/githubRoutes');
-<<<<<<< HEAD
-const session = require('express-session');
-const passport = require('passport');
-const googleAuthRoutes = require('./routes/googleAuth');
-
-=======
->>>>>>> recovered-dashboard
 app.use(express.json());
 const allowedOrigins = ["http://localhost:5173","http://localhost:3000"];
 
@@ -34,16 +27,9 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-<<<<<<< HEAD
-app.use("/auth", authRoutes);
-app.use("/auth", googleAuthRoutes); // Add this line
-app.use("/team", teamRoutes);
-app.use("/api", githubRoutes);
-=======
 app.use("/auth",authRoutes);
 app.use("/team",teamRoutes);
 app.use("/api",githubRoutes);
->>>>>>> recovered-dashboard
 app.listen(process.env.PORT, () => {
     console.log(`http://localhost:${process.env.PORT}`);
     }
